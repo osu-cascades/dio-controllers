c45c8b67a6119aa38d9f5e721f4ed484129e30e0
Makeila Lundy <makeila@pixelandtonic.com>
2017-11-06 19:53:18 -0800

    Contuned work on Wifi sending data

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # dio-controllers
+## HardWare needed
+* Arduino Uno Board
+* Half BreadBoard
+* Dissovled Oxygen Sensor
+* Xbee Radio
+* Wifi Shield
 
-# Sensor Documentation
-<!-- https://storage.googleapis.com/bc3_production_blobs/f88509d6-b9ea-11e7-ae3a-a0369f6beb07?GoogleAccessId=bc3-production-storage%40bc3-production.iam.gserviceaccount.com&Expires=1508983629&Signature=cVgnaNlJ1lmpYw1jLVu9jYQuACtU5Mn8gad9qKdPGEeGfEzgm2yqGEO1%2FgpeErshVy%2BW7yxgLL%2Bt86kWHBLtY%2BzwJY0VgzW0vTb0udknhVcjX35SPDqkwdUzdCvzu%2F6m%2B0kqqixoaWjkZBdw5Wsh91t53eVuDCzOftkeGnNGhjTD7FhrHz3cChi7RCrnKtNoWKJQmHwvfTuzWrPZJQGgh%2FOMI3tCtoQgqWkUoY9r3Ideyh4%2FnVWJgA0Vi67PDrQ0adEb1HJ9e%2FDTkwCHFZOj5%2Bc8f2t%2BagX%2FcFWqkBeSDza0WaZsRpNHee6lPhGnhEGrjfFs9IL4ghvf5EoOXDjrlA%3D%3D&response-content-type=application%2Fpdf&response-content-disposition=inline%3B+filename%3D%22DO_EZO_Datasheet.pdf%22%3B+filename%2A%3DUTF-8%27%27DO_EZO_Datasheet.pdf -->
+[Sensor Documentation]( https://storage.googleapis.com/bc3_production_blobs/f88509d6-b9ea-11e7-ae3a-a0369f6beb07?GoogleAccessId=bc3-production-storage%40bc3-production.iam.gserviceaccount.com&Expires=1508983629&Signature=cVgnaNlJ1lmpYw1jLVu9jYQuACtU5Mn8gad9qKdPGEeGfEzgm2yqGEO1%2FgpeErshVy%2BW7yxgLL%2Bt86kWHBLtY%2BzwJY0VgzW0vTb0udknhVcjX35SPDqkwdUzdCvzu%2F6m%2B0kqqixoaWjkZBdw5Wsh91t53eVuDCzOftkeGnNGhjTD7FhrHz3cChi7RCrnKtNoWKJQmHwvfTuzWrPZJQGgh%2FOMI3tCtoQgqWkUoY9r3Ideyh4%2FnVWJgA0Vi67PDrQ0adEb1HJ9e%2FDTkwCHFZOj5%2Bc8f2t%2BagX%2FcFWqkBeSDza0WaZsRpNHee6lPhGnhEGrjfFs9IL4ghvf5EoOXDjrlA%3D%3D&response-content-type=application%2Fpdf&response-content-disposition=inline%3B+filename%3D%22DO_EZO_Datasheet.pdf%22%3B+filename%2A%3DUTF-8%27%27DO_EZO_Datasheet.pdf)
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 
 
 #include <SoftwareSerial.h>
+#include <Xbee.h>
 #include <Arduino.h>                         //we have to include the SoftwareSerial library, or else we can't use it
 #define rx 2                                          //define what pin rx is going to be
 #define tx 3                                          //define what pin tx is going to be
 
-//SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+SoftwareSerial Xbee(rx, tx);                      //define how the soft serial port is going to work
+XBee xbee = XBee();
 
 
 String inputstring = "";                              //a string to hold incoming data from the PC
@@ -25,11 +27,11 @@ SoftwareSerial xbee(rx,tx);                                      //used to hold 
 
 
 void setup() {                                        //set up the hardware
-//  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
-//  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
+  //myserial.begin(9600);                               //set baud rate for the software serial port to 9600
   inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
   sensorstring.reserve(30);
-  xbee.begin(9600);
+  Xbee.begin(9600);
 
                           //set aside some bytes for receiving data from Atlas Scientific product
 }
@@ -44,18 +46,18 @@ void serialEvent() {                                  //if the hardware serial p
 void loop() {                                         //here we go...
 
   if (input_string_complete){                         //if a string from the PC has been received in its entirety
-  //myserial.print(inputstring);
-  xbee.print(inputstring);                     //send that string to the Atlas Scientific product
-  //myserial.print('\r');
-  xbee.print('\r');                            //add a <CR> to the end of the string
+myserial.print(inputstring);
+  xbee.write(inputstring);                     //send that string to the Atlas Scientific product
+  myserial.print('\r');
+  xbee.write('\r');                            //add a <CR> to the end of the string
     inputstring = "";                                 //clear the string
     input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
   }
 
-  //if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+//if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
   if (xbee.available() > 0) {
-//  char inchar = (char)myserial.read();              //get the char we just received
-  char inchar = (char)xbee.read();
+  char inchar = (char)myserial.read();              //get the char we just received
+  //char inchar = (char)xbee.read();
     sensorstring += inchar;                           //add the char to the var called sensorstring
     if (inchar == '\r') {                             //if the incoming character is a <CR>
       sensor_string_complete = true;                  //set the flag
@@ -64,8 +66,8 @@ void loop() {                                         //here we go...
 
 
   if (sensor_string_complete== true) {                //if a string from the Atlas Scientific product has been received in its entirety
-  //  Serial.println(sensorstring);                     //send that string to the PC's serial monitor
-    xbee.println(sensorstring);
+   Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+  //  xbee.println(sensorstring);
                                                     //uncomment this section to see how to convert the DO reading from a string to a float
     // if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
     //   DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
