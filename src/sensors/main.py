66e678b49a697376da7f401f99f2ce1169525dde
Makeila Lundy <makeila@pixelandtonic.com>
2018-03-07 18:07:00 -0800

    trouble shooting

--- src/sensors/main.py
+++ src/sensors/main.py
@@ -1,21 +1,21 @@
+#!/usr/bin/python
+
 from i2c import AtlasI2C
 
 addr1 = 97
 addr2 = 99
 addr3 = 100
-
 delaytime = 6
 
-def main():
-		atlas = AtlasI2C()
-		atlas.address(addr1)
-		print('address 1 = ',addr1)
-		atlas.poll(delaytime)
-		atlas.address(addr2)
-		print('address 2 = ',addr2)
-		atlas.poll(delaytime)
-		atlas.address(addr3)
-		print('address 3 = ',addr3)
-		atlas.poll(delaytime)
+atlas = AtlasI2C()
+atlas.address(addr1)
+print('address 1 = ',addr1)
+atlas.poll(delaytime)
+atlas.address(addr2)
+print('address 2 = ',addr2)
+atlas.poll(delaytime)
+atlas.address(addr3)
+print('address 3 = ',addr3)
+atlas.poll(delaytime)
 
 	
